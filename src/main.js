90e3ddba6eb0e6abc5b01d19bb03a8034cc0ec0e
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-25 18:21:06 -0300

    Creating Vue project with vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
+
+  // This function expects a template as a parameter to be compiled, in this case 'App'.
+  // The Vue instance is being built on an element with 'app' id in the index.html.
   render: h => h(App)
 }).$mount('#app')
