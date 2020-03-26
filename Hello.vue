78c57224822350d5cd7461f51c964d45667f7cea
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-26 09:22:36 -0300

    Adding single file component to use instant prototyping

---
+++ Hello.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1> {{ title }}</h1>
+        <input v-model="title" >
+    </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'Hello VueJs'
+    }
+  }
+}
+</script>
